cf3b649f1d451c7a36fda48ca38e172dc9d5bd2a
Joaquin Mouxaut <joaquinmouxaut@gmail.com>
2022-01-14 12:36:40 -0300

    rediseño primera parte

--- src/js/app.js
+++ src/js/app.js
@@ -1,27 +1,28 @@
 document.addEventListener('DOMContentLoaded', function(){
     scrollNav();
-
-    navegacionFija();
+    // navegacionFija();
+    iniciarHero();
+    eventListeners();
 });
 
-function navegacionFija() {
-    const navBg = document.querySelector('.nav-bg');
-    const navegacion = document.querySelector('.navegacion-principal');
+// function navegacionFija() {
+//     const navBg = document.querySelector('.nav-bg');
+//     const navegacion = document.querySelector('.navegacion-principal');
 
-    //Registrar el InterseccionObserver
-    const observer = new IntersectionObserver(function(entries) {
-        if(entries[0].isIntersecting) {
-            navBg.classList.remove('fijo');
-            navegacion.classList.remove('fijo');
-        } else {
-            navBg.classList.add('fijo');
-            navegacion.classList.add('fijo');
-        }
-    });
+//     //Registrar el InterseccionObserver
+//     const observer = new IntersectionObserver(function(entries) {
+//         if(entries[0].isIntersecting) {
+//             navBg.classList.remove('fijo');
+//             navegacion.classList.remove('fijo');
+//         } else {
+//             navBg.classList.add('fijo');
+//             navegacion.classList.add('fijo');
+//         }
+//     });
 
-    //Elemento a Observar
-    observer.observe(document.querySelector('.titulo'));
-}
+//     //Elemento a Observar
+//     observer.observe(document.querySelector('.titulo'));
+// }
 
 function scrollNav() {
     const enlaces = document.querySelectorAll('.navegacion-principal a');
@@ -37,4 +38,30 @@ function scrollNav() {
         });
     });
     
+}
+
+function iniciarHero() {
+    const contenido = document.querySelector( '.contenido-video' );
+    const navbar = document.querySelector( '.navegacion-principal' );
+    const mobilMenu = document.querySelector( '.mobil-menu' );
+    setTimeout(() => {
+        contenido.classList.add( 'mostrar' );
+        if( screen.width > 768 ) {
+            navbar.classList.add( 'mostrar' );
+        } else {
+            mobilMenu.classList.add( 'mostrar' );
+        }
+    }, 1000);
+}
+
+function eventListeners() {
+    const mobilMenu = document.querySelector( '.mobil-menu' );
+    mobilMenu.addEventListener( 'click', navegacionResponsive );
+}
+
+function navegacionResponsive() {
+    const navegacionMobil = document.querySelector( '.navegacion-mobil' );
+    const contenido = document.querySelector( '.contenido-video' );
+    navegacionMobil.classList.toggle( 'desplegar' );
+    contenido.classList.toggle( 'mostrar' );
 }
